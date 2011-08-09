728c63aa647896b1bcbdc9a9f71da960767f73c9
Johannes Buchner <buchner.johannes@gmx.at>
2011-08-09 20:42:40 +1200

    proper doc and pom info

--- maven-jython-compile-plugin-test/src/main/java/InitJython.java
+++ maven-jython-compile-plugin-test/src/main/java/InitJython.java
@@ -1,14 +1,11 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.LineNumberReader;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 import jline.ConsoleReader;
-
-import org.python.util.JLineConsole;
 
 /**
  * The point of all the jython install madness is that we can call jython now.
